e523254960240a68f95d145b94c715b2d2c46b8b
bartekkrok <bartlomiej.krok@b-connected.io>
2025-10-24 07:52:38 +0200

    fixes

--- src/components/Discography.tsx
+++ src/components/Discography.tsx
@@ -2,61 +2,8 @@
 
 import ScrollStack from "@/components/ScrollStack";
 
-const ToksycznyIframe = () => {
-    return <iframe
-        data-testid="embed-iframe"
-        style={{borderRadius: "12px"}}
-        src="https://open.spotify.com/embed/track/6yRyOXOi8aZyT1BQFIAses?utm_source=generator"
-        width="100%"
-        height="352"
-        frameBorder="0"
-        allowFullScreen
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-    />
-}
-
-const GhostingIframe = () => {
-    return <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}}
-                   src="https://open.spotify.com/embed/track/3P4u2xwspyWcwN6BUt1YAw?utm_source=generator"
-                   width="100%" height="352" frameBorder="0" allowFullScreen=""
-                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                   loading="lazy"></iframe>
-}
-const ChceZapomniecIframe = () => {
-    return <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}}
-                   src="https://open.spotify.com/embed/track/2pOe7Ambcy8TooJUazmBwC?utm_source=generator" width="100%"
-                   height="352" frameBorder="0" allowFullScreen=""
-                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                   loading="lazy"></iframe>
-}
-const NoweRozdanieIframe = () => {
-    return <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}}
-                   src="https://open.spotify.com/embed/track/1ZPYNtCNTDXj4tJzaXCQWS?utm_source=generator&theme=0"
-                   width="100%" height="352" frameBorder="0" allowFullScreen=""
-                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                   loading="lazy"></iframe>
-}
 const Discography = () => {
-    const myCards = [
-        {
-            id: 1,
-            bg: "bg-red-500",
-            iframe: ToksycznyIframe
-        },
-        {
-            id: 2, bg: "bg-blue-500",
-            iframe: GhostingIframe
-        },
-        {
-            id: 3, bg: "bg-green-500",
-            iframe: ChceZapomniecIframe
-        },
-        {
-            id: 4, bg: "bg-yellow-500",
-            iframe: NoweRozdanieIframe
-        },
-    ];
+
 
     return (
         <section className="py-24 md:px-8 max-w-7xl mx-auto" id="gallery">
@@ -65,7 +12,7 @@ const Discography = () => {
                 <div className="h-1 w-24 bg-gradient-accent mx-auto"/>
             </div>
 
-            <ScrollStack cards={myCards}/>;
+            <ScrollStack/>;
         </section>
     );
 };
--- src/components/ScrollStack.tsx
+++ src/components/ScrollStack.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import React, {JSX, useRef} from "react";
+import React, {useRef} from "react";
 import {motion, MotionValue, useScroll, useTransform} from "framer-motion";
 
-interface Card {
+export interface Card {
     id: number | string;
-    bg: string;
-    iframe: JSX.Element
+    iframe: React.ReactNode
 }
 
 interface StackParams {
@@ -25,6 +24,56 @@ const DEFAULT_PARAMS: StackParams = {
     scaleBack: 0.95,
 };
 
+const ToksycznyIframe = () => <iframe
+    data-testid="embed-iframe"
+    style={{borderRadius: "12px"}}
+    src="https://open.spotify.com/embed/track/6yRyOXOi8aZyT1BQFIAses?utm_source=generator"
+    width="100%"
+    height="352"
+    frameBorder="0"
+    allowFullScreen
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+    loading="lazy"
+/>
+
+
+const GhostingIframe = () => <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}}
+                                     src="https://open.spotify.com/embed/track/3P4u2xwspyWcwN6BUt1YAw?utm_source=generator"
+                                     width="100%" height="352" frameBorder="0" allowFullScreen
+                                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                                     loading="lazy"></iframe>
+
+const ChceZapomniecIframe = () => <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}}
+                                          src="https://open.spotify.com/embed/track/2pOe7Ambcy8TooJUazmBwC?utm_source=generator"
+                                          width="100%"
+                                          height="352" frameBorder="0" allowFullScreen
+                                          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                                          loading="lazy"></iframe>
+
+const NoweRozdanieIframe = () => <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}}
+                                         src="https://open.spotify.com/embed/track/1ZPYNtCNTDXj4tJzaXCQWS?utm_source=generator&theme=0"
+                                         width="100%" height="352" frameBorder="0" allowFullScreen
+                                         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                                         loading="lazy"></iframe>
+
+const cards = [
+    {
+        id: 1,
+        iframe: ToksycznyIframe
+    },
+    {
+        id: 2,
+        iframe: GhostingIframe
+    },
+    {
+        id: 3,
+        iframe: ChceZapomniecIframe
+    },
+    {
+        id: 4,
+        iframe: NoweRozdanieIframe
+    },
+];
 const useCardAnimation = (
     scrollYProgress: MotionValue<number>,
     index: number,
@@ -58,11 +107,10 @@ const useCardAnimation = (
 
 
 interface ScrollStackProps {
-    cards: Card[];
     params?: StackParams;
 }
 
-export const ScrollStack: React.FC<ScrollStackProps> = ({cards, params}) => {
+const ScrollStack: React.FC<ScrollStackProps> = ({params}) => {
     const containerRef = useRef<HTMLDivElement | null>(null);
     const numCards = cards.length;
     const stackParams = params || DEFAULT_PARAMS;
@@ -97,9 +145,17 @@ export const ScrollStack: React.FC<ScrollStackProps> = ({cards, params}) => {
                                 width: "70%",
                                 height: 352,
                                 margin: "auto",
+                                overflow: 'hidden',
                                 transformOrigin: "top center", // ważne, żeby wjeżdżając w stack karta rosła od góry
+                                boxShadow: `
+                                  0 0 12px rgba(215, 66, 165, 0.6),     /* kolor bordera glow */
+                                  0 10px 25px rgba(0, 0, 0, 0.4),       /* miękka głębia pod spodem */
+                                  0 30px 60px rgba(0, 0, 0, 0.5)        /* głęboki cień */
+                                `,
+
+                                backdropFilter: "blur(4px)"
                             }}
-                            className={`flex items-center justify-center rounded-2xl shadow-xl ${card.bg}`}
+                            className={`flex items-center justify-center rounded-2xl shadow-xl`}
                         >
                             <IframeComponent/>
                         </motion.div>
