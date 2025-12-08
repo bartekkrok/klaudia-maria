e52960552b1bc5e3b4c30393ee52a73b046e7623
bartekkrok <bartlomiej.krok@b-connected.io>
2025-12-08 23:06:46 +0100

    fix sizing for hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,14 +11,14 @@ const Hero = () => {
         className="absolute left-0 bottom-0 opacity-65 max-md:w-1/2 max-md:h-auto"
       />
       <div className="relative z-10 text-center px-4 max-w-4xl mx-auto fade-in">
-        <h1 className="text-6xl md:text-8xl font-bold mb-6 tracking-tight  font-(family-name:--font-playfair)">
+        <h1 className="text-6xl max-md:text-5xl font-bold mb-6 tracking-tight  font-(family-name:--font-playfair)">
           Klaudia-Maria
         </h1>
-        <div className="h-1 w-24 bg-gradient-accent mx-auto mb-8" />
-        <p className="text-xl md:text-2xl tracking-wide">
+        <div className="h-1 w-24 bg-gradient-accent mx-auto mb-8 max-md:mb-5" />
+        <p className="text-xl md:text-xl tracking-wide">
           Wokalistka • Autorka piosenek • Marzycielka
         </p>
-        <p className="mt-6 text-lg  max-w-2xl mx-auto font-body">
+        <p className="mt-6 max-md:mt-3 text-lg md:text-xl max-w-2xl mx-auto font-body">
           Pisze i śpiewa o tym, co czuje - prawdziwie i z duszą.
         </p>
       </div>
